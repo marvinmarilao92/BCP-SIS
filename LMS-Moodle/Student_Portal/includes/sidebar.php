ef5cfb2243a9e34ec234cb91c8fdefde3a311735
kerkss <gsmabarrai@gmail.com>
2022-03-31 23:13:36 +0800

    Edited on dynamic log-in, added lesson butto
    Edited on dynamic log-in
    added lesson button
    added students button
    added lesson function
    edited header title

---
+++ LMS-Moodle/Student_Portal/includes/sidebar.php
@@ -0,0 +1,27 @@
+<!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="index.php">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#clearance-students-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-person-lines-fill"></i><span>Clearance For Students</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="clearance-students-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="clearance-status.php">
+              <i class="bi bi-circle"></i><span>Clearance Status</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End clearance-students Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
--- SIS_LandingPage/dynamic-login.php
+++ SIS_LandingPage/dynamic-login.php
@@ -213,8 +213,26 @@
                  case "Health Check Monitoring":
                    //statement
                    break;
-                 case "LMS Moodle":
+                 case "LMS Moodle": 
                    //statement
+                   switch($row1["role"]){
+                    case "LMS Administrator":
+                      //statement
+                      $_SESSION['session_username'] = $myusername;
+                      if (!empty($_SERVER["HTTP_CLIENT_IP"])){
+                          $ip = $_SERVER["HTTP_CLIENT_IP"];
+                        }elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
+                          $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+                        }else{
+                          $ip = $_SERVER["REMOTE_ADDR"];
+                          $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+                           $remarks="account has been logged in";  
+                           mysqli_query($link,"INSERT INTO audit_logs(user_id,account_no,action,action_name,ip,host,login_time) VALUES('$id','$admin','$remarks','$fname','$ip','$host','$date')")or die(mysqli_error($link));
+                        }
+                      header("location: ../LMS-Moodle/Manager/index.php");
+                      break;                                           
+                      }
+
                    break;
                  case "Medical System":
                    //statement
