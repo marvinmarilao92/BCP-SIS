92649d83f54238af7f411d67ab50f05b1c8d2659
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-04-01 21:36:38 +0800

    Database configuration update

--- Document-Approval-Tracking-and-Mngt-System/DATMS/admission/include/conn.php
+++ Document-Approval-Tracking-and-Mngt-System/DATMS/admission/include/conn.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect("localhost","root","","sis_db");
+$conn = mysqli_connect("localhost","u692894633_sis_db","l95o@WMN6~a","u692894633_sis_db");
 
 if(!$conn){
 	die("Connection error: " . mysqli_connect_error());	
--- Document-Approval-Tracking-and-Mngt-System/DATMS/approver/include/config.php
+++ Document-Approval-Tracking-and-Mngt-System/DATMS/approver/include/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'sis_db');
+define('DB_USERNAME', 'u692894633_sis_db');
+define('DB_PASSWORD', 'l95o@WMN6~a');
+define('DB_NAME', 'u692894633_sis_db');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
