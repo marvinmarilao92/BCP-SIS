9e3bb3b5875061866b141bd87ebbeaf2fd7d2c84
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-04-04 17:10:27 +0800

    Update tracking_docx.php

--- Document-Approval-Tracking-and-Mngt-System/DATMS/approver/function/tracking_docx.php
+++ Document-Approval-Tracking-and-Mngt-System/DATMS/approver/function/tracking_docx.php
@@ -1,143 +1,149 @@
 <?php
-//fetch.php
-$connect = mysqli_connect("localhost", "u692894633_sis_db", "l95o@WMN6~a", "u692894633_sis_db");
-$output = '';
-$error = '';
-$idenifier='';
-$d1;
-$d2;
-if(isset($_POST["query"]))
-{
- $search = mysqli_real_escape_string($connect, $_POST["query"]);
- $query = " SELECT * FROM datms_tracking WHERE doc_code = '".$search."'";
-}
-else
-{
- $query = "SELECT * FROM datms_tracking ORDER BY doc_code";
-}
-$result = mysqli_query($connect, $query);
-if(mysqli_num_rows($result) > 0)
-{
- while($row = mysqli_fetch_array($result))
- {
-   //array data
-    $doc_code = $row["doc_code"];
-    $doc_title = $row["doc_title"];
-    $doc_name = $row["doc_name"];
-    $doc_type = $row["doc_type"];
-    $doc_status = $row["doc_status"];
-    $doc_desc = $row["doc_desc"];
-    $doc_actor1 = $row["doc_actor1"];
-    $doc_off1 = $row["doc_off1"];
-    $doc_date1 = $row["doc_date1"];
-    $doc_actor2 = $row["doc_actor2"];
-    $doc_off2 = $row["doc_off2"];
-    $doc_date2 = $row["doc_date2"];
-    $doc_remarks = $row["doc_remarks"];
-
-    $d1 = $row["doc_date1"];
-    $d2 = $row["doc_date2"];
-     // Declare and define two dates
-     $date1 = strtotime("$d1");
-     $date2 = strtotime("$d2");
-
-     // Formulate the Difference between two dates
-     $diff = abs($date2 - $date1);
-   
-     // To get the year divide the resultant date into
-     // total seconds in a year (365*60*60*24)
-     $years = floor($diff / (365*60*60*24));
-   
-     // To get the month, subtract it with years and
-     // divide the resultant date into
-     // total seconds in a month (30*60*60*24)
-     $months = floor(($diff - $years * 365*60*60*24)
-                                   / (30*60*60*24));
-   
-     // To get the day, subtract it with years and
-     // months and divide the resultant date into
-     // total seconds in a days (60*60*24)
-     $days = floor(($diff - $years * 365*60*60*24 -
-                 $months*30*60*60*24)/ (60*60*24));
-   
-     // To get the hour, subtract it with years,
-     // months & seconds and divide the resultant
-     // date into total seconds in a hours (60*60)
-     $hours = floor(($diff - $years * 365*60*60*24
-           - $months*30*60*60*24 - $days*60*60*24)
-                                       / (60*60));
-   
-     // To get the minutes, subtract it with years,
-     // months, seconds and hours and divide the
-     // resultant date into total seconds i.e. 60
-     $minutes = floor(($diff - $years * 365*60*60*24
-             - $months*30*60*60*24 - $days*60*60*24
-                               - $hours*60*60)/ 60);
-   
-     // To get the minutes, subtract it with years,
-     // months, seconds, hours and minutes
-     $seconds = floor(($diff - $years * 365*60*60*24
-             - $months*30*60*60*24 - $days*60*60*24
-                     - $hours*60*60 - $minutes*60));
-           
-     if($years !=0 ){
-       // Print the result
-       $duration = "$years"." yr,";
-     }else if($months != 0 ){
-       $duration = "$months"." mos";
-     }else if($days != 0 ){
-       $duration = "$days"." days";
-     }else if($hours != 0 ){
-       $duration = "$hours"." hrs";
-     }else if($minutes != 0 ){
-       $duration = "$minutes"." min";
-     }else if($seconds != 0 ){
-       $duration = "$seconds"." sec";
-     }else if($seconds == 0 ){
-      $duration = "1"." sec";
-    }else{
-       $duration = "2";
-     }
-     if ($doc_status =='Created'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-primary align-self-start" style="padding-top: 25px;"></i>';
-     }else if($doc_status =='Outgoing'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-warning align-self-start" style="padding-top: 25px;"></i>';
-     }else if($doc_status =='Received'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-success align-self-start" style="padding-top: 25px;"></i>';
-     }else if($doc_status =='Approved'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-info align-self-start" style="padding-top: 25px;"></i>';
-     }else if($doc_status =='Rejected'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-danger align-self-start" style="padding-top: 25px;"></i>';
-     }else if($doc_status =='Deleted'){
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-dark align-self-start" style="padding-top: 25px;"></i>';
-     }else{
-      $idenifier=' <i class="bi bi-circle-fill activity-badge text-muted align-self-start" style="padding-top: 25px;"></i>';
-     }
-
-  $output .= '
-    <div class="activity-item d-flex">
-      <div class="activite-label card-title">'.$duration.'</div>
-     
-      '.$idenifier.'
-
-      <div class="activity-content card-title" >
-      <a class="text-dark " title="Date: '.$doc_date1.'">'. $doc_remarks .'</a> <a class="fw-bold text-dark ">'.$doc_actor1.'</a> at <a class="fw-bold text-dark">'. $doc_off1.'</a>
-      </div>
-    </div>  
-    
-  ';
-  
- }
- echo $output;
-}
-else
-{
-  $error .='
-    <div class="alert alert-danger border-light alert-dismissible fade show" role="alert">
-      No document to track in our database or you input incomplete code
-    </div>
-  ';
- echo $error;
-}
-
+include('session.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<title>DATMS | Tracking Documents</title>
+<head>
+<?php include ('core/css-links.php');//css connection?>
+
+</head>
+<body>
+
+<?php include ('core/header.php');//Design for  Header?>
+<?php $page = 'track';  include ('core/side-nav.php');//Design for sidebar?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      
+      <h1>Track Documents</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item">Module</li>
+          <li class="breadcrumb-item active">Track Document</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+        <!-- Left side columns -->
+          <div class="row">
+
+            <!-- Reports -->
+          
+              <div class="card">
+                <!-- Activity Body -->
+                <div class="card-body">
+                  <!-- Search Bar -->
+
+                    <div class="col-md-12" style="margin-top: 30px; margin-bottom: 10px;">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="search_text" name="search_text" onChange="fetchTracking(this.value);" placeholder="Your Name" autofocus>
+                            <label for="floatingName">Enter Code</label>
+                          </div>
+                    </div>  
+
+                  <!-- End of search Bar -->
+                    <!-- Tracking Activity module -->
+                    <div class="activity">
+
+                        <!-- <div class="activity-item d-flex">
+                          <div class="activite-label card-title">20 min</div>
+                          <i class='bi bi-circle-fill activity-badge text-danger align-self-start' style="padding-top: 25px;"></i>
+                          <div class="activity-content card-title" >
+                             Document set Outgoing by <a href="#" class="fw-bold text-dark">Juvell Alquero</a> at <a href="#" class="fw-bold text-dark">Accounting Office</a> 
+                          </div>
+                        </div>
+                        
+                        <div class="activity-item d-flex">
+                          <div class="activite-label card-title">10 min</div>
+                          <i class='bi bi-circle-fill activity-badge text-warning align-self-start' style="padding-top: 25px;"></i>
+                          <div class="activity-content card-title" >
+                              Document set Incoming by <a href="#" class="fw-bold text-dark">Marvin Marilao</a> at <a href="#" class="fw-bold text-dark">CCS Office</a> 
+                          </div>
+                        </div>
+                        
+                        <div class="activity-item d-flex">
+                          <div class="activite-label card-title">15 min</div>
+                          <i class='bi bi-circle-fill activity-badge text-info align-self-start' style="padding-top: 25px;"></i>
+                          <div class="activity-content card-title" >
+                            Document set Approved by <a href="#" class="fw-bold text-dark">Rogel Quintero</a> at <a href="#" class="fw-bold text-dark">VP Office</a> 
+                          </div>
+                        </div>
+                        
+                        <div class="activity-item d-flex">
+                          <div class="activite-label card-title">20 min</div>
+                          <i class='bi bi-circle-fill activity-badge text-success align-self-start' style="padding-top: 25px;"></i>
+                          <div class="activity-content card-title" >
+                            Document set Received by <a href="#" class="fw-bold text-dark">Rogel Quintero</a> at <a href="#" class="fw-bold text-dark">VP Office</a> 
+                          </div>
+                        </div>
+                       
+                        <div class="activity-item d-flex">
+                          <div class="activite-label card-title">90 min</div>
+                          <i class='bi bi-circle-fill activity-badge text-muted align-self-start' style="padding-top: 25px;"></i>
+                          <div class="activity-content card-title" >
+                            Document set Rejected by <a href="#" class="fw-bold text-dark">Rogel Quintero</a> at <a href="#" class="fw-bold text-dark">VP Office</a> 
+                          </div>
+                        </div> -->
+                   
+                    </div>
+                    <!-- End Tracking Activity module -->
+                </div>
+              <!-- End Activity Body -->
+              </div>
+            </div>
+
+          </div>
+
+      
+    </section>
+  </main><!-- End #main -->
+
+ 
+  <!-- ======= Footer ======= -->
+  <?php include ('core/footer.php');//css connection?>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files/ Template main js file -->
+  <?php include ('core/js.php');//css connection?>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+  <script type="text/javascript">
+      $(document).ready(function(){
+
+      // load_data();
+
+      function load_data(query)
+      {
+        $.ajax({
+        url:"function/tracking_docx.php",
+        method:"POST",
+        data:{query:query},
+        success:function(data)
+        {
+          $('.activity').html(data);
+        }
+        });
+      }
+      $('#search_text').keyup(function(){
+        var search = $(this).val();
+        if(search != '')
+        {
+        load_data(search);
+        }
+        else
+        {
+          $('.activity').html('');
+        }
+      });
+      });
+  </script>
+</html>
