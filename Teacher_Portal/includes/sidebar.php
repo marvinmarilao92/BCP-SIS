3a984e1f48fab03c40ffe8729ceac54b603a891e
olivertjr17 <olivertjr17@gmail.com>
2022-04-18 21:19:47 +0800

    update

--- Teacher_Portal/includes/sidebar.php
+++ Teacher_Portal/includes/sidebar.php
@@ -6,9 +6,25 @@
       <li class="nav-item">
         <a class="<?php if($page=='dash'){echo 'nav-link';}else{echo 'nav-link collapsed';}?>" href="index.php">
           <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
+          <span>News Feed</span>
         </a>
       </li><!-- End Dashboard Nav -->
+
+      <li class="nav-heading">Clearance</li>
+
+        <!-- Audit Trail -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-people"></i><span>Clearance For Teachers</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="components-nav" class="<?php if($col=='Clearance'){echo 'nav-content collapse show';}else{echo 'nav-content collapse';}?> " data-bs-parent="#sidebar-nav">
+          <li>
+              <a href="clearance-status.php" class="<?php if($page=='clr'){echo 'active';}?>">
+                <i class="bi bi-circle"></i><span>Clearance Status</span>
+              </a>
+            </li>
+          </ul>
+        </li><!-- End Account Management Nav -->
 
       <li class="nav-heading">Document Tracking</li>
       
