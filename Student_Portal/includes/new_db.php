f9005bd76d71dcf95dcd59f168b0828120ea5363
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-04-20 18:48:09 +0800

    Reconfig

--- Student_Portal/includes/new_db.php
+++ Student_Portal/includes/new_db.php
@@ -1,9 +1,10 @@
 <?php
+// domain config
 class DB{
     protected $host='localhost';
-    protected $user='root';
-    protected $password='';
-    protected $database='sis_db';
+    protected $user='u692894633_sis_db';
+    protected $password='l95o@WMN6~a';
+    protected $database='u692894633_sis_db';
 
     public $conn;
 
@@ -15,6 +16,24 @@ class DB{
         }
     }
 }
+
+// local config
+// class DB{
+//     protected $host='localhost';
+//     protected $user='root';
+//     protected $password='';
+//     protected $database='sis_db';
+
+//     public $conn;
+
+//     public function __construct()
+//     {
+//         $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
+//         if($this->conn->error){
+//             die('Database error');
+//         }
+//     }
+// }
 
 
 
