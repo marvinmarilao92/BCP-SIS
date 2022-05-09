b630b965f6e5f1cc5ba01b08fd6d746ed2de2fa6
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-05-09 22:40:33 +0800

    password checker

--- Document-Approval-Tracking-and-Mngt-System/DATMS/admin/changePassword/openButton.php
+++ Document-Approval-Tracking-and-Mngt-System/DATMS/admin/changePassword/openButton.php
@@ -2,9 +2,9 @@
 if (isset($_SESSION["conpass"]) && isset($_SESSION["newpass"]) &&  $_SESSION["conpass"] == true && $_SESSION["newpass"] == true &&
 isset( $_SESSION["oldpass"]) &&  $_SESSION["oldpass"] == true)
 {
-  echo '<button type="submit" id="submit" class="btn btn-primary">Change Password</button>';
+  echo '<button type="submit" id="save" class="btn btn-primary">Change Password</button>';
 }
 else {
-  echo '<button type="submit" disabled class="btn btn-primary">Change Password</button>';
+  echo '<button type="submit" id="save" disabled class="btn btn-primary">Change Password</button>';
 }
  ?>
