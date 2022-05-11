e158ff83ffe9f929778734496e24005be9d573bf
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-05-12 01:45:22 +0800

    Update view_template.php

--- Student_Portal/function/view_template.php
+++ Student_Portal/function/view_template.php
@@ -5,7 +5,7 @@ require_once("../includes/conn.php");
 
 if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn,$_GET['id']);
-
+    
     // fetch file to download from database
     $sql = "SELECT * FROM  datms_tempreq WHERE id = $id";
     $result = mysqli_query($conn, $sql);
@@ -14,17 +14,9 @@ if (isset($_GET['id'])) {
     $filepath = '../../assets/request/' . $file['file_name'];
 
     if (file_exists($filepath)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=' . basename($filepath));
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize('../../assets/request/' . $file['file_name']));
+        header('Content-Type: application/pdf');
         readfile('../../assets/request/' . $file['file_name']);
 
-        // Now update downloads count        
-        $updateQuery = "UPDATE datms_tempreq SET status='Downloaded' WHERE id=$id";
         mysqli_query($conn, $updateQuery);
         exit;
     }
