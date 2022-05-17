bd70f8ef9e86bb7484d09cca7da5ccfcbcf258bb
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-05-17 23:41:19 +0800

    Revert "Merge branch 'Clearance' into Document-Approval_Tranking-And-Mngt-System"
    This reverts commit e7276e190028088f7872497b09114aded0056533, reversing
    changes made to 2cd4aff0206b7c6f9b7cd28a41f6d044fe3d69cb.

--- Clearance/clearance-coordinator/includes/config.php
+++ Clearance/clearance-coordinator/includes/config.php
@@ -1,24 +1,24 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u692894633_sis_db');
-define('DB_PASSWORD', 'l95o@WMN6~a');
-define('DB_NAME', 'u692894633_sis_db');
-
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// Check connection
-if($link === false){  
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-// /* Database credentials. Assuming you are running MySQL
-// server with default setting (user 'root' with no password) */
 // define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'sis_db');
+// define('DB_USERNAME', 'u692894633_sis_db');
+// define('DB_PASSWORD', 'l95o@WMN6~a');
+// define('DB_NAME', 'u692894633_sis_db');
+
+// /* Attempt to connect to MySQL database */
+// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// // Check connection
+// if($link === false){  
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'sis_db');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -27,4 +27,3 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
-
