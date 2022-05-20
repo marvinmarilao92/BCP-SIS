0fcee794417b4ad77335c0dfbfbdf2369c9d7eff
Hwuwjw <vincejalbo01@gmail.com>
2022-05-20 23:07:58 +0800

    hehe

---
+++ IMS/LOCAL/PATH/STUDENT-INTERN-PANEL/auth/check.php
@@ -0,0 +1,64 @@
+<?php 
+include 'config.php';
+
+function validate($data){
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+}
+                  if(isset($_POST['submit'])) {
+
+  
+                   $uname = validate($_POST['uname']);
+                   $pass = validate($_POST['password']);
+                   $role = 'Student';
+                        $sql = "SELECT *FROM ims_dummy_free_users 
+                        INNER JOIN student_information
+                        ON 
+                        ims_dummy_free_users.id = student_information.id
+                        WHERE 
+                        ims_dummy_free_users.username='$uname' 
+                        AND
+                        ims_dummy_free_users.role='$role'";
+
+                        $result = mysqli_query($conn, $sql);
+  
+                        //checking row
+                        if (mysqli_num_rows($result) > 0) {
+                                    //if true execute tong baba
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                    //oop var check row ['pass'] ==> db
+                                    $oop = password_verify($pass,$row['password']);
+                                          //pag ung oop var nag true = 1 execute yan baba true means 1 ,false = 0 => "Location: ../login.php?error=Invalid login, Please try again"
+                                          if ($oop == 1)
+                                          {
+                                                session_start();
+                                                $_SESSION['logged'] = true;
+                                                $_SESSION['num'] = $row['id_number'];
+                                                $_SESSION['role'] = $row['role'];
+                                                $_SESSION['fname'] = $row['firstname'];
+                                                $_SESSION['mname'] = $row['middlename'];
+                                                $_SESSION['lname'] = $row['lastname'];
+                                                $_SESSION['email'] = $row['email'];
+                                                $_SESSION['dp'] = $row['stud_img'];
+                                                header("location: ../");
+                                                die();
+                                          }
+                                          else{
+                                                header("Location: ../login.php?error=Invalid login, Please try again");
+                                                die();
+                                          }
+                                    }
+                        }
+                        else{
+                              header("Location: ../login.php?error=Invalid login, Please try again");
+                              die();
+                        }
+                  }
+                  else{
+                  header("Location: system/..");
+                  die();
+                  }
+
+?>
---
+++ IMS/LOCAL/SECTION/PAGE/drawer/section.php
@@ -0,0 +1,5 @@
+<div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+            <h1>Learning Today,<br>Leading Tomorrow</h1>
+            <h2></h2>
+            <a href="courses.html" class="btn-get-started">Get Started</a>
+        </div>
