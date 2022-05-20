8d6e5fe638b29843573c429dfb95dbf594e74df2
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-05-20 18:42:55 +0800

    Reconfigure

--- Document-Approval-Tracking-and-Mngt-System/DATMS/admission/include/new_db.php
+++ Document-Approval-Tracking-and-Mngt-System/DATMS/admission/include/new_db.php
@@ -1,24 +1,6 @@
 <?php
 // domain config
-class DB{
-   protected $host='localhost';
-   protected $user='u692894633_sis_db';
-   protected $password='l95o@WMN6~a';
-   protected $database='u692894633_sis_db';
-
-   public $conn;
-
-   public function __construct()
-   {
-       $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
-       if($this->conn->error){
-           die('Database error');
-      }
-   }
-}//
-
-// local config
-//  class DB{
+// class DB{
 //    protected $host='localhost';
 //    protected $user='u692894633_sis_db';
 //    protected $password='l95o@WMN6~a';
@@ -31,9 +13,27 @@ class DB{
 //        $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
 //        if($this->conn->error){
 //            die('Database error');
-//        }
+//       }
 //    }
 // }
+
+// local config
+ class DB{
+   protected $host='localhost';
+   protected $user='u692894633_sis_db';
+   protected $password='l95o@WMN6~a';
+   protected $database='u692894633_sis_db';
+
+   public $conn;
+
+   public function __construct()
+   {
+       $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
+       if($this->conn->error){
+           die('Database error');
+       }
+   }
+}
 
 
 
--- Help-Desk-System/Program/include/new_db.php
+++ Help-Desk-System/Program/include/new_db.php
@@ -1,39 +1,39 @@
 <?php
 // domain config
-// class DB{
-//    protected $host='localhost';
-//    protected $user='u692894633_sis_db';
-//    protected $password='l95o@WMN6~a';
-//    protected $database='u692894633_sis_db';
-
-//    public $conn;
-
-//    public function __construct()
-//    {
-//        $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
-//        if($this->conn->error){
-//            die('Database error');
-//        }
-//    }
-// }
-
-// local config
- class DB{
+class DB{
    protected $host='localhost';
-   protected $user='root';
-protected $password='';
-    protected $database='sis_db';
+   protected $user='u692894633_sis_db';
+   protected $password='l95o@WMN6~a';
+   protected $database='u692894633_sis_db';
 
    public $conn;
 
    public function __construct()
-    {
-        $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
+   {
+       $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
        if($this->conn->error){
-            die('Database error');
-     }
-    }
- }
+           die('Database error');
+       }
+   }
+}
+
+// local config
+//  class DB{
+//    protected $host='localhost';
+//    protected $user='root';
+// protected $password='';
+//     protected $database='sis_db';
+
+//    public $conn;
+
+//    public function __construct()
+//     {
+//         $this->conn=new mysqli($this->host,$this->user,$this->password,$this->database);
+//        if($this->conn->error){
+//             die('Database error');
+//      }
+//     }
+//  }
 
 
 
