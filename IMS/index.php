1a8b9595d309e814a77115227a74a0e90ee57219
marvinmarilao92 <81927108+marvinmarilao92@users.noreply.github.com>
2022-05-21 15:30:21 +0800

    Change Buttons for intern

--- IMS/index.php
+++ IMS/index.php
@@ -1,77 +1,161 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<title>SuperUser | Home</title>
 <head>
-<?php include 'drawer/header.php' ?>
+<?php include ('core/css-links.php');//css connection?>
 </head>
 
 <body>
 
-
-              <div class="modal fade" id="largeModal" tabindex="-1">
-                <div class="modal-dialog modal-lg">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Quick Access Panel</h5>
-                      
-                    </div>
-                    <div class="modal-body" style="text-align: center;">
-                    <a href="../LOCAL/CASHIER/login.php"> <button type="button" class="btn btn-secondary">Cashier Panel</button></a>
-                    <a href="../LOCAL/REGISTRAR/login.php?"> <button type="button" class="btn btn-primary">Registrar Panel</button></button></a>
-                    <a href="../LOCAL/PATH/STUDENT-INTERN-PANEL/login.php"> <button type="button" class="btn btn-secondary">Intern Panel</button></a>
-                    <a href="../LOCAL/PATH/DEPARTMENT-COORDINATOR-PANEL/login.php?"> <button type="button" class="btn btn-primary">Coordinator Panel</button></button></a>
-                    <a href="../LOCAL/DEPARTMENT-ADMIN-PANEL/login.php?"> <button type="button" class="btn btn-primary">Admin Panel</button></button></a>
-                    <a href="../LOCAL/SECTION/LOGIN/LOGIN/login.php?"> <button type="button" class="btn btn-primary">Company Panel</button></button></a>
-                    
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                   
-                  </div>
-                </div>
-              </div>
-  <!-- ======= Header ======= -->
- <!-- End Header -->
-
-  <!-- ======= Sidebar ======= -->
-  <!--<aside id="sidebar" class="sidebar">
-  
-  <?//php include 'drawer/sidebar.php'?>
+  <main style="padding: 20px;">
     
-
-  </aside> End Sidebar-->
-  <br>
-  <br>
-  <main>
-    <div class="container">
-
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-xxl-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-              <!-- End Logo -->
-
-              
-             
-
+    <div class="pagetitle">
+      <h1>Internship Quick Access Panel</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <br>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+    <div class="pagetitle">
+      <h1>School Information System</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index">Home</a></li>
+          <li class="breadcrumb-item active">School Information System</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+    <section class="section dashboard">
+      <div class="row align-items-top">
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="CASHIER/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Cashier Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
             </div>
           </div>
         </div>
+        <!-- End Insert your Module here -->
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="REGISTRAR/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Registrar Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Insert your Module here -->
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="PATH/STUDENT-INTERN-PANEL/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Intern Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Insert your Module here -->
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="PATH/DEPARTMENT-COORDINATOR-PANEL/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Coordinator Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Insert your Module here -->
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="DEPARTMENT-ADMIN-PANEL/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Admin Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Insert your Module here -->
+        <!-- Insert your Module here -->
+        <div class="col-lg-2">
+          <div class="card">
+            <a href="SECTION/LOGIN/LOGIN/login.php"><img src="../assets/img/mvcampus.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+              <h5 class="card-title">Company Panel</h5>
+              <p class="card-text">Subsystem Description insert here...</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Insert your Module here -->
 
-      </section>
+          <!-- End Insert your Module here -->
+      </div>
+    </section>
 
-    </div>
-  </main> <!-- End #main -->
+  </main><!-- End #main -->
+  <hr>
+    <!-- ======= Footer ======= -->
+    <footer class="footer">
+      <div class="copyright" style="margin-bottom: 30px;">
+        <center>
+          &copy;Copyright <a href="https://bcp.edu.ph/home" target="_blank " data-bs-toggle="tooltip" data-bs-placement="top" 
+          title="Access BCP Website">Bestlink College of the Philippines</a> All Rights Reserved
+        </center>                 
+      </div>
+    </footer>
+    <!-- End Footer -->
 
-  <!-- ======= Footer ======= -->
-  
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <?php require'drawer/js.php' ?>
-
+  <!-- Vendor JS Files/ Template main js file -->
+  <?php include ('core/js.php');//css connection?>
+<!-- Charts -->
 </body>
 
+  <!-- prevent you for turning back -->
+  <script>
+    (function (global) {
+
+      if(typeof (global) === "undefined") {
+          throw new Error("window is undefined");
+      }
+
+      var _hash = "!";
+      var noBackPlease = function () {
+          global.location.href += "#";
+
+          // Making sure we have the fruit available for juice (^__^)
+          global.setTimeout(function () {
+              global.location.href += "!";
+          }, 50);
+      };
+
+      global.onhashchange = function () {
+          if (global.location.hash !== _hash) {
+              global.location.hash = _hash;
+          }
+      };
+
+      global.onload = function () {
+          noBackPlease();
+
+          // Disables backspace on page except on input fields and textarea..
+          document.body.onkeydown = function (e) {
+              var elm = e.target.nodeName.toLowerCase();
+              if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                  e.preventDefault();
+              }
+              // Stopping the event bubbling up the DOM tree...
+              e.stopPropagation();
+          };
+      }
+    })(window);
+  </script>
 </html>
