adf940800b9865cb83fa3e68c2367a7c1ce771ab
Nelson Asumbrado <84433704+doedoe123-boop@users.noreply.github.com>
2022-05-31 18:02:19 +0800

    adding new chat

--- Student_Portal/pages-faq.php
+++ Student_Portal/pages-faq.php
@@ -165,6 +165,18 @@ include('includes/session.php');
  <?php include 'includes/footer.php'; ?>
   <?php include ("view_ticket.php"); ?>
 
- 
+ <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6295e69eb0d10b6f3e74e4fa/1g4copmuv';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
   </body>
 </html>
