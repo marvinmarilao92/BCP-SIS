26fc7e15f936d54a8364fe132abf3ae0620271be
leynampersand <104402382+KBLangMalakas@users.noreply.github.com>
2022-06-02 01:14:30 +0800

    ms
    ms ajax trial

--- super_admin/include/config.php
+++ super_admin/include/config.php
@@ -6,18 +6,6 @@ server with default setting (user 'root' with no password) */
 // define('DB_PASSWORD', 'l95o@WMN6~a');
 // define('DB_NAME', 'u692894633_sis_db');
 
-<<<<<<< Updated upstream
-// /* Attempt to connect to MySQL database */
-// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// // Check connection
-// if($link === false){  
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
-
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-=======
->>>>>>> Stashed changes
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -26,6 +14,6 @@ define('DB_NAME', 'sis_db');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
-if($link === false){  
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($link === false) {
+  die("ERROR: Could not connect. " . mysqli_connect_error());
 }
