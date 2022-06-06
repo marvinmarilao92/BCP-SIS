864aca644bafd158da945744fd77a7d5d5c9f2f3
Hwuwjw <vincejalbo01@gmail.com>
2022-06-06 22:53:19 +0800

    ......

---
+++ COMPANY/2022/PORTAL/constant/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername= "localhost:3307";
+$db_user= "root";
+$db_pass = "";
+$db_name = "sis_db";
+
+$link = mysqli_connect($servername, $db_user, $db_pass, $db_name);
+
+
+ if($link === false){
+    die("ERROR: Could not connect. ".mysqli_connect_error());
+ } 
+ 
+?>
