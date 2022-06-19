ccab2455c5823da9dd61bdbea4cb388b2e1547b6
Hwuwjw <vincejalbo01@gmail.com>
2022-06-20 03:41:51 +0800

    ....

--- COMPANY/2022/auth/ges.php
+++ COMPANY/2022/auth/ges.php
@@ -1,5 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+
+require '../vendor/autoload.php';
 
 
 if(isset($_POST['submit']) && isset($_FILES['uploaded_file'])){
@@ -51,13 +56,14 @@ function register_as_company()
 				$password_2 = 	validate($_POST['re_pass']); 
 				$password = PASSWORD_HASH($password_1, PASSWORD_DEFAULT);
 				
-
+				
 
 				
 
 				$fname =       	validate($_POST['fname']);
 				$mname =       	validate($_POST['midname']);	
 				$lname =       	validate($_POST['lname']);
+				
 				$paddress =       	validate($_POST['presentadd']);
 				$contact =       	validate($_POST['contact']);	
 				$email =       	validate($_POST['r_email']);
@@ -80,29 +86,74 @@ function register_as_company()
 				
 
 		        
-				
-                $run = mysqli_query($conn,$sql) or die(mysqli_error());
+	
+         $run = mysqli_query($conn,$sql) or die(mysqli_error());
 
 				$run2 = mysqli_query($conn,$sql2) or die(mysqli_error());
 				if($run && $run2)
 				{	
 					 
-					mysqli_query($conn,	"INSERT INTO `ims_files` (`uid`,`name`, `mime`, `size`, `data`, `created`)
+							mysqli_query($conn,	"INSERT INTO `ims_files` (`uid`,`name`, `mime`, `size`, `data`, `created`)
 		            VALUES ('{$i_no}','{$name}', '{$mime}', '{$size}', '{$data}', NOW())") or die(mysqli_error($conn));
-					echo '<br>';
-					echo '<h1>Account Details </h1>';
-					echo '<br>';
-					echo "Please copy ur autogen Username ! ";
-					echo '<br>';
-					echo "Hello ur auto generated username : ".$autogen_reg;
-					echo '<br>';
-					echo "Account Created : ".$date;
-					echo '<br>';	
-					echo '<br>';
-					echo '<a href = "../">back to Register </a>';
-					echo '</table>';
-					
-					die();
+
+					 // $success='Your ticket has been created. Make sure to check your email inbox for ticket ID';
+                      $mail = new PHPMailer;
+                      $mail->isSMTP();
+                      $mail->SMTPDebug = 0;                                       //Send using SMTP
+                      $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
+                      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                      $mail->Username   = 'internship_system@bcp-sis.ga';           //SMTP username
+                      $mail->Password   = '#Internship01!';                         //SMTP password
+                      $mail->SMTPSecure = 'TLS';                                  //Enable implicit TLS encryption
+                      $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+              
+                      //Recipients
+                      $mail->setFrom('internship_system@bcp-sis.ga', 'Internship Technical Support');
+                      $mail->addAddress($email);//Add a recipient
+                      $mail->AddReplyTo('internship_system@bcp-sis.ga', "No Reply"); // indicates ReplyTo headers
+                    
+                    $body = 
+                      "<div class='card'>          
+                        <div class='card-body'>
+                          <h5 class='card-title'></h5>
+                          
+                          <br>Hi ".$rname."<br><br>
+                          
+
+                          		Account Details <br>
+
+                          		Auto-gen Username : " .$autogen_reg. "<br>
+                          						 Password : " .$password_1. " <br>		
+                          						 	 Status : " .$status. "<br><br>
+
+
+
+
+
+                          
+                          This email is sent from an account we use for sending messages only. So if<br>
+                          you want to contact us, don't reply to this email-we won't get your<br>
+                          response. Instead, Go to Registrar office to comply.<br>
+                          <br>Thank you! Stay safe.</p>
+                        </div>
+                      </div>
+                      
+                      <div class='alert alert-light bg-light border-0 alert-dismissible fade show' role='alert'>
+                        © Copyright Bestlink College of the Philippines. All Rights Reserved.
+                      </div>             
+                    ";
+                      $subject = 'Account Successfully Created';
+                      //Content
+                      $mail->isHTML(true); //Set email format to HTML
+                      $mail->Subject = $subject;
+                      $mail->Body    = $body;
+                      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                      
+                     if($mail->send()){
+                        echo ('success');
+                      }else{
+                        echo ('failed');
+                      }
 				}
 				else{
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
--- COMPANY/2022/auth/regis.php
+++ COMPANY/2022/auth/regis.php
@@ -1,7 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 
-
+require '../function/vendor/autoload.php';
 if(isset($_POST['submit'])){
 
 	register_as_company();
@@ -57,8 +60,54 @@ function register_as_company()
 				if($run && $ru2)
 				{
 					
-					
-					echo '<br>';
+					// $success='Your ticket has been created. Make sure to check your email inbox for ticket ID';
+                      $mail = new PHPMailer;
+                      $mail->isSMTP();
+                      $mail->SMTPDebug = 0;                                       //Send using SMTP
+                      $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
+                      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                      $mail->Username   = 'internship_system@bcp-sis.ga';           //SMTP username
+                      $mail->Password   = '#Internship01!';                         //SMTP password
+                      $mail->SMTPSecure = 'TLS';                                  //Enable implicit TLS encryption
+                      $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+              
+                      //Recipients
+                      $mail->setFrom('internship_system', 'Internship Technical Support');
+                      $mail->addAddress($cemail);//Add a recipient
+                      $mail->AddReplyTo('internship_system@bcp-sis.ga', "No Reply"); // indicates ReplyTo headers
+                    
+                    $body = 
+                      "<div class='card'>          
+                        <div class='card-body'>
+                          <h5 class='card-title'></h5>
+                          <p class='card-text'>This is direct message from Registrar Department<br>
+                          <br>Dear".$repname."<br><br>
+                          This email is sent from an account we use for sending messages only. So if<br>
+                          you want to contact us, don't reply to this email-we won't get your<br>
+                          response. Instead, Go to Registrar office to comply.<br>
+                          <br>Thank you! Stay safe.</p>
+                        </div>
+                      </div>
+                      
+                      <div class='alert alert-light bg-light border-0 alert-dismissible fade show' role='alert'>
+                        © Copyright Bestlink College of the Philippines. All Rights Reserved.
+                      </div>             
+                    ";
+                      
+                      //Content
+                      $mail->isHTML(true); //Set email format to HTML
+                      $mail->Subject = $subject;
+                      $mail->Body    = $body;
+                      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                      
+                      
+                      if($mail->send()){
+                        echo ('success');
+                      }else{
+                        echo ('failed');
+                      }
+                        //echo "Dear Student your tickets has been sent to our help desk support team and we will back to you shortly. and here is your ticket id $unid please keep your ticket id";
+					/*echo '<br>';
 					echo '<h1>Account Details </h1>';
 					echo '<br>';
 					echo "Please copy ur autogen Username ! ";
@@ -71,7 +120,7 @@ function register_as_company()
 					echo '<a href = "../">back to Register </a>';
 					echo '</table>';
 					
-					die();
+					die();*/
 				}
 				else{
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
